d9a9e5cb0aaf01b896b827019d1bed453497df03
olivier <salfran.olivier@gmail.com>
2018-11-14 09:41:30 +0100

    Option gray

--- src/Controller/OptionController.java
+++ src/Controller/OptionController.java
@@ -1,8 +1,13 @@
 package Controller;
 
+import com.sun.javaws.jnl.JavaFXAppDesc;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 
 import javax.swing.*;
 
@@ -12,7 +17,8 @@ public class OptionController {
     public void leaveGame(){
         System.exit(0);
     }
-    public void changeTailleEcranOption(){
+
+    public void changeTailleEcranOption(Event event){
         System.out.println("Je change de fenetre");
     }
 }
