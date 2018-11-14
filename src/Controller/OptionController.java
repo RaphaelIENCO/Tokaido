e30b16f030baec30e22d20eaaa1c4e097de90334
RaphaelIENCO <raphaelienco@gmail.com>
2018-11-14 09:59:58 +0100

    Style boutons du chemin et fonction de selection.

--- src/Controller/OptionController.java
+++ src/Controller/OptionController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
@@ -20,5 +21,18 @@ public class OptionController {
 
     public void changeTailleEcranOption(Event event){
         System.out.println("Je change de fenetre");
+    }
+
+    public void selection(ActionEvent a){
+        Button b;
+        b = (Button)a.getSource();
+        b.setStyle(
+                "-fx-background-radius: 5em; " +
+                        "-fx-min-width: 30px; " +
+                        "-fx-min-height: 30px; " +
+                        "-fx-max-width: 30px; " +
+                        "-fx-max-height: 30px;" +
+                        "-fx-background-color: #be1d00"
+        );
     }
 }
