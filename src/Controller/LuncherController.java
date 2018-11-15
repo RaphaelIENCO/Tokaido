cf873b3eaa77e6ba2688472a9f14a81b5fab3526
RaphaelIENCO <raphaelienco@gmail.com>
2018-11-15 22:08:31 +0100

    Ajout luncher v1

---
+++ src/Controller/LuncherController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.util.Objects;
+
+public class LuncherController {
+
+
+    @FXML
+    public void startGame(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("Vue/plateau.fxml")));
+        Scene scene = new Scene(root);
+        Stage secondaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        secondaryStage.setScene(scene);
+        secondaryStage.show();
+    }
+}
--- src/Vue/Main.java
+++ src/Vue/Main.java
@@ -18,13 +18,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("plateau.fxml"));
-        primaryStage.setTitle("Tokaido");
-        primaryStage.setScene(new Scene(root, 1500, 550));
-        primaryStage.setMaximized(true);
+        Parent root = FXMLLoader.load(getClass().getResource("luncher.fxml"));
+        primaryStage.setTitle("LuncherV1 Tokaido");
+        primaryStage.setScene(new Scene(root, 500, 550));
+        primaryStage.setMaximized(false);
         primaryStage.setResizable(true);
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
