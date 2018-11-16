62f7b05f608618f0b9d031ab1fac60f1b4e39af9
alphas70 <jeannoel.robert70@gmail.com>
2018-11-16 10:12:31 +0100

    Création de la liste des cartes repas

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,29 +1,33 @@
 package Model;
 
+import javafx.print.Collation;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Model {   // classe dans laquel on initie toutes les carte du joueur.
-    ArrayList<Rencontre>listRecontre = new ArrayList<Rencontre>();
-    ArrayList<Repas>listRepas  = new ArrayList<>();
+    private ArrayList<Rencontre>listRecontre = new ArrayList<Rencontre>();
+    private ArrayList<Repas>listRepas  = new ArrayList<>();
+    private ArrayList<Souvenirs>listSouvenir = new ArrayList<>();
+
     public Model(){
         initPartie();
     }
 
     public void initPartie(){ //Creation de toutes les differentes cartes du jeu
         initRencontre();
+        Collections.shuffle(listRecontre);
         initRepas();
+        Collections.shuffle(listRepas);
         initSouvenir();
+        Collections.shuffle(listSouvenir);
     }
 
     public void initRencontre(){ //Init de toutes les rencontre
 
     }
 
-    public void initRepas(){
-
-    }
-
-    public void initSouvenir(){
+    public void initRepas(){ //Init de touts les repas
         for(int i=0 ; i<3 ;i++){
             listRepas.add(new Repas("Misoshiru",1,6));
             listRepas.add(new Repas("Nigirineshi",1,6));
@@ -42,6 +46,36 @@ public class Model {   // classe dans laquel on initie toutes les carte du joueu
         listRepas.add(new Repas("Fugu",3,6));
         listRepas.add(new Repas("TaiMeshi",3,6));
         listRepas.add(new Repas("Sashini",3,6));
+    }
+
+    public void initSouvenir(){ //Init de touts les souvenirs
+        listSouvenir.add(new Souvenirs("Yunomi",1,1));
+        listSouvenir.add(new Souvenirs("Gofu",1,1));
+        listSouvenir.add(new Souvenirs("Koma",1,1));
+        listSouvenir.add(new Souvenirs("Hashi",1,1));
+        listSouvenir.add(new Souvenirs("Uchina",1,1));
+        listSouvenir.add(new Souvenirs("Washi",1,1));
+
+        listSouvenir.add(new Souvenirs("Kompeito",1,2));
+        listSouvenir.add(new Souvenirs("Manju",1,2));
+        listSouvenir.add(new Souvenirs("Kamaboko",1,2));
+        listSouvenir.add(new Souvenirs("Daiguku",2,2));
+        listSouvenir.add(new Souvenirs("Ucha",2,2));
+        listSouvenir.add(new Souvenirs("Sake",2,2));
+
+        listSouvenir.add(new Souvenirs("Sandogasa",2,3));
+        listSouvenir.add(new Souvenirs("Yukata",2,3));
+        listSouvenir.add(new Souvenirs("Furoshiki",2,3));
+        listSouvenir.add(new Souvenirs("Geta",2,3));
+        listSouvenir.add(new Souvenirs("KanZashi",2,3));
+        listSouvenir.add(new Souvenirs("Haori",2,3));
+
+        listSouvenir.add(new Souvenirs("Metsuke",2,4));
+        listSouvenir.add(new Souvenirs("Jubako",2,4));
+        listSouvenir.add(new Souvenirs("Shikki",2,4));
+        listSouvenir.add(new Souvenirs("Shanisen",3,4));
+        listSouvenir.add(new Souvenirs("Sumie",3,4));
+        listSouvenir.add(new Souvenirs("Ukiyoe",3,4));
     }
 
 }
