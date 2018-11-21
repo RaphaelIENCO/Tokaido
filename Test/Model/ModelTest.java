3d52cbc16263de9cf6c2dfe31f1ee91d8fede903
alphas70 <jeannoel.robert70@gmail.com>
2018-11-21 15:42:07 +0100

    Création de test pour couvrir le model

--- Test/Model/ModelTest.java
+++ Test/Model/ModelTest.java
@@ -42,4 +42,64 @@ public class ModelTest {
         Model model = new Model();
         Assert.assertEquals(12,model.getListSource().size());
     }
+
+    @Test
+    public void testListRepasNonNull(){
+        Model model = new Model();
+        boolean verif = true;
+        for (int i=0; i<model.getListRepas().size() ; i++){
+            if (model.getListRepas().get(i) == null) verif=false;
+        }
+        Assert.assertTrue(verif);
+    }
+
+    @Test
+    public void testListSouvenirNonNull(){
+        Model model = new Model();
+        boolean verif = true;
+        for (int i=0; i<model.getListSouvenir().size() ; i++){
+            if (model.getListSouvenir().get(i) == null) verif=false;
+        }
+        Assert.assertTrue(verif);
+    }
+
+    @Test
+    public void testListSourceNonNull(){
+        Model model = new Model();
+        boolean verif = true;
+        for (int i=0; i<model.getListSource().size() ; i++){
+            if (model.getListSource().get(i) == null) verif=false;
+        }
+        Assert.assertTrue(verif);
+    }
+
+    @Test
+    public void testListPanoRiNonNull(){
+        Model model = new Model();
+        boolean verif = true;
+        for (int i=0; i<model.getListPanoramaRiziere().size() ; i++){
+            if (model.getListPanoramaRiziere().get(i) == null) verif=false;
+        }
+        Assert.assertTrue(verif);
+    }
+
+    @Test
+    public void testListPanoMoNonNull(){
+        Model model = new Model();
+        boolean verif = true;
+        for (int i=0; i<model.getListPanoramaMontagnes().size() ; i++){
+            if (model.getListPanoramaMontagnes().get(i) == null) verif=false;
+        }
+        Assert.assertTrue(verif);
+    }
+
+    @Test
+    public void testListPanoMeNonNull(){
+        Model model = new Model();
+        boolean verif = true;
+        for (int i=0; i<model.getListPanoramaMer().size() ; i++){
+            if (model.getListPanoramaMer().get(i) == null) verif=false;
+        }
+        Assert.assertTrue(verif);
+    }
 }
